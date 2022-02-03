65ef17a7c1487ce8d00841796d0ecc7f36732b4c
Kartikey Sharma <kartikey.bluebells@gmail.com>
2022-02-03 11:32:50 +0530

    templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-@app.route('/helloworld')
 def index():
-	return "Hello World!"
+	user= {'username':'Kartikey'}
+	posts= [
+		{
+			'author': {'username':'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username':'Kartikey'},
+			'body': 'Raining in New Delhi!'
+		},
+		{
+			'author': {'username':'Bob'},
+			'body': 'Gonna sleep :)'
+		}	
+	]
+	return render_template('index.html',title='Home',user=user,posts=posts)
