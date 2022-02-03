8973c643e7c943e59bdff9de78aa3e483ce50909
Kartikey Sharma <kartikey.bluebells@gmail.com>
2022-02-03 16:40:06 +0530

    init db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
 
+#SQL ALCHEMY IMPORTS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app= Flask(__name__)
 app.config.from_object(Config)
+# DB inits
+db= SQLAlchemy(app)
+migrate= Migrate(app,db)
 
-from app import routes
+from app import routes, models
